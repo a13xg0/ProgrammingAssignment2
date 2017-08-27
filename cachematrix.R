296b573138282e6d2f1196ba71a1583f23539b14
Aleksandr Gorbach <a.v.gorbach@gmail.com>
2017-08-27 19:23:50 +1000

    Cached matrix inversion package

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This package covers a functinality of creating
+## a cached matrix object and perform matrix inversion.
+## Cahced Matrix inversion result will be stored in the 
+## package cache to prevent a redundant computation.
 
-## Write a short comment describing this function
+## Function generates an object of cached matrix
+## with additional functions to manage internal variables
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	# inverse of the matrix
+	i <- NULL
+    set <- function(y) {
+    	x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+ 	   setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Caluclates inversion of the cahced matrix object and store
+## result in the cahce. At next requests function will return
+## value from the object's internal cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
